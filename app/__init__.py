1909193186f8b4cdc3bcf4058204bec2d546cbbb
scottmiao <jazzymiao@gmail.com>
2014-01-22 16:45:05 +0800

    Logging out
    v

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
@@ -8,11 +8,9 @@ from config import basedir
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+lm = LoginManager()
+lm.setup_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
-
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'  # Flask-Login needs to know what view logs users in.
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
